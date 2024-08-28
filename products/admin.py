db0a4d8d9fcf6b29b8191f89fa8d76014288fb59
Jayesh C.J <jayeshcj2003@gmail.com>
2024-08-28 13:29:37 +0530

    Changed admin panel view to add productimages along with products

--- products/admin.py
+++ products/admin.py
@@ -4,4 +4,15 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Product)
+
+
+# Changing admin panel view to upload ProductImages with Product
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
+
+
+admin.site.register(Product, ProductAdmin)
